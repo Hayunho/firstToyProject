8e63ff92c4b0a045d60807ec6c736bc04cc753ef
Hayunho <yunho081544@gmail.com>
2024-01-29 17:31:02 +0900

    토이 프로젝트 파일 생성
    - 미니게임 앱 제작
    - 비밀번호 맞추기
    - 가위바위보

---
+++ toyProject/toyProject/toyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  toyProjectApp.swift
+//  toyProject
+//
+//  Created by 하윤호 on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct toyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
