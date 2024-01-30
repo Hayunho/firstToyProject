a2359406668936ab70525e79084c9458766e09b7
Hayunho <yunho081544@gmail.com>
2024-01-30 23:48:58 +0900

    contentView 코드 수정
    - 커스텀 수정자 추가
    - 버튼 스타일, 글꼴

--- toyProject/toyProject/ContentView.swift
+++ toyProject/toyProject/ContentView.swift
@@ -8,53 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    let screenWidth = UIScreen.main.bounds.size.width
-    let screenHeight = UIScreen.main.bounds.size.height
-    
     var body: some View {
         VStack {
-            Spacer()
-            
-            Text("미니게임")
-                .font(.system(size: 72))
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            
-            Spacer()
-            Spacer()
-            
-            Button {
-                
-            } label: {
-                ZStack {
-                    Rectangle()
-                        .fill(Color("MainColor"))
-                        .frame(width: screenWidth * 0.80, height:screenHeight * 0.07)
-                        .cornerRadius(30.0)
-                    Text("가위바위보")
-                        .foregroundColor(.black)
-                        .fontWeight(.medium)
-                        .font(.system(size: 40))
-                }
+            VStack{
+                Text("미니게임")
+                    .font(.system(size: 72))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             }
             
-            Button {
+            Spacer()
+            
+            VStack{
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color("MainColor"))
+                            .modifier(MiniGameButtonStyle())
+                        Text("가위바위보")
+                            .modifier(MiniGameButtonTitle())
+                    }
+                }
                 
-            } label: {
-                ZStack {
-                    Rectangle()
-                        .fill(Color("MainColor"))
-                        .frame(width: screenWidth * 0.80, height:screenHeight * 0.07)
-                        .cornerRadius(30.0)
-                    Text("비밀번호")
-                        .foregroundColor(.black)
-                        .fontWeight(.medium)
-                        .font(.system(size: 40))
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color("MainColor"))
+                            .modifier(MiniGameButtonStyle())
+                        Text("비밀번호")
+                            .modifier(MiniGameButtonTitle())
+                    }
                 }
             }
-            
             Spacer()
         }
         .padding()
+    }
+}
+
+struct MiniGameButtonStyle: ViewModifier {
+    private let screenWidth = UIScreen.main.bounds.size.width
+    private let screenHeight = UIScreen.main.bounds.size.height
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: screenWidth * 0.80, height:screenHeight * 0.07)
+            .cornerRadius(30.0)
+    }
+}
+
+struct MiniGameButtonTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.black)
+            .fontWeight(.medium)
+            .font(.system(size: 40))
     }
 }
 
