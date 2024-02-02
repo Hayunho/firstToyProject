64a4975ffdc1e2f5bae104e1947aa6695047a5d2
Hayunho <yunho081544@gmail.com>
2024-02-03 00:34:10 +0900

    fix: SecretNumberModel  및 View 리펙토링
    - ScretNumberModel 메소드 추가
    - View ForEach문 삭제
    - View subVIew 생성

--- toyProject/toyProject/Models/SecretNumberModel.swift
+++ toyProject/toyProject/Models/SecretNumberModel.swift
@@ -26,4 +26,47 @@ struct SecretNumberModel {
         self.thirdNumber = Int.random(in: 0...9)
         self.forthNumber = Int.random(in: 0...9)
     }
+    
+    func compareNumber(seleted: Int, number: Int) -> String {
+        var result: String = ""
+        
+        switch(seleted) {
+        case 1:
+            if number == firstNumber {
+                result = "GOOD"
+            } else if number < firstNumber {
+                result = "UP"
+            } else {
+                result = "DOWN"
+            }
+        case 2:
+            if number == secondNumber {
+                result = "GOOD"
+            } else if number < secondNumber {
+                result = "UP"
+            } else {
+                result = "DOWN"
+            }
+        case 3:
+            if number == thirdNumber {
+                result = "GOOD"
+            } else if number < thirdNumber {
+                result = "UP"
+            } else {
+                result = "DOWN"
+            }
+        case 4:
+            if number == forthNumber {
+                result = "GOOD"
+            } else if number < forthNumber {
+                result = "UP"
+            } else {
+                result = "DOWN"
+            }
+        default:
+            break
+        }
+        
+        return result
+    }
 }
